a9604cd3257303866c633dc529c597757995157d
Jihyun <hwiyu25@gmail.com>
2021-03-20 17:23:16 +0900

    #3.0 - #3.2 CSS in React

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -5,12 +5,14 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
+import Header from "Components/Header";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
 
 export default () => (
   <Router>
+    <Header />
     <Switch>
       <Route path="/" exact={true} component={Home} />
       <Route path="/tv" exact component={TV} />
