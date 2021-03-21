67f1c3c593bfd18f277b9fb7a1582547e24b16c1
Jihyun <hwiyu25@gmail.com>
2021-03-21 15:44:14 +0900

    #5.3 TV Container

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,3 +1,4 @@
+import { tvApi } from "api";
 import React from "react";
 import TVPresenter from "./TVPresenter";
 
@@ -9,8 +10,35 @@ export default class extends React.Component {
     error: null,
     loading: true,
   };
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: airingToday },
+      } = await tvApi.airingToday();
+      const {
+        data: { results: topRated },
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular },
+      } = await tvApi.popular();
+      this.setState({
+        airingToday,
+        topRated,
+        popular,
+      });
+    } catch (error) {
+      this.setState({
+        error: "Can't find TV show information",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
   render() {
     const { airingToday, topRated, popular, error, loading } = this.state;
+    console.log(this.state);
     return (
       <TVPresenter
         airingToday={airingToday}
