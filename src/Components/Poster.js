a428f3f27cc39a915a7e517b9015f17987cd8005
Jihyun <hwiyu25@gmail.com>
2021-03-23 14:45:13 +0900

    #6.7 Poster Component part Two

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -3,23 +3,61 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  font-size: 12px;
+`;
 
-const ImageContainer = styled.div``;
+const Image = styled.div`
+  height: 180px;
+  background-image: url(${(props) => props.bgUrl});
+  background-size: cover;
+  background-position: center center;
+  border-radius: 4px;
+  transition: opacity 0.1s linear;
+`;
 
-const Image = styled.div``;
+const Rating = styled.span`
+  position: absolute;
+  bottom: 5px;
+  right: 10px;
+  opacity: 0;
+  transition: opacity 0.1s linear;
+`;
 
-const Rating = styled.span``;
+const ImageContainer = styled.div`
+  position: relative;
+  margin-bottom: 5px;
+  &:hover {
+    ${Image} {
+      opacity: 0.3;
+    }
+    ${Rating} {
+      opacity: 1;
+    }
+  }
+`;
 
-const Title = styled.span``;
+const Title = styled.span`
+  display: block;
+  margin-bottom: 2px;
+`;
 
-const Year = styled.span``;
+const Year = styled.span`
+  font-size: 10px;
+  color: rgba(255, 255, 255, 0.5);
+`;
 
 const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
   <Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
     <Container>
       <ImageContainer>
-        <Image bgUrl={imageUrl} />
+        <Image
+          bgUrl={
+            imageUrl
+              ? `http://image.tmdb.org/t/p/w300${imageUrl}`
+              : `https://github.com/nomadcoders/nomflix/blob/2b3839f5d0adc1f6e9ea7d126285ac81c53cea82/src/assets/noPosterSmall.png?raw=true`
+          }
+        />
         <Rating>
           <span role="img" aria-label="rating">
             ⭐
@@ -27,7 +65,9 @@ const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
           {rating}/10
         </Rating>
       </ImageContainer>
-      <Title>{title}</Title>
+      <Title>
+        {title.length > 18 ? `${title.substring(0, 18)}...` : title}
+      </Title>
       <Year>{year}</Year>
     </Container>
   </Link>
--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,8 +1,65 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "Components/Loader";
 
-const DetailPresenter = ({ result, error, loading }) => null;
+const Container = styled.div`
+  position: relative;
+  height: calc(100vh - 50px);
+  width: 100%;
+  margin-top: -30px;
+  padding: 50px;
+`;
+
+const Backdrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  filter: blur(2px);
+  opacity: 0.5;
+  z-index: 0;
+`;
+
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`;
+
+const Cover = styled.div`
+  width: 30%;
+  height: 100%;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  border-radius: 5px;
+  z-index: 1;
+`;
+
+const DetailPresenter = ({ result, error, loading }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      <Backdrop
+        bgImage={`http://image.tmdb.org/t/p/original${result.backdrop_path}`}
+      />
+      <Content>
+        <Cover
+          bgImage={
+            result.poster_path
+              ? `http://image.tmdb.org/t/p/original${result.poster_path}`
+              : `https://github.com/nomadcoders/nomflix/blob/2b3839f5d0adc1f6e9ea7d126285ac81c53cea82/src/assets/noPosterSmall.png?raw=true`
+          }
+        />
+      </Content>
+    </Container>
+  );
 
 DetailPresenter.propTypes = {
   result: PropTypes.array,
